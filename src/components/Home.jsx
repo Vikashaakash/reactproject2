02fd8acffbf7fed4bdb0e545fdd767d0c235fdeb
VIKASH KUMAR <94210571+Vikashaakash@users.noreply.github.com>
2024-03-20 16:45:06 +0530

    Add files via upload

---
+++ src/components/Home.jsx
@@ -0,0 +1,116 @@
+import { Box, Container, Heading, Image, Stack, Text } from '@chakra-ui/react';
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import img1 from '../assets/1.jpg';
+import img2 from '../assets/2.jpg';
+import img3 from '../assets/3.jpg';
+import img4 from '../assets/4.jpg';
+import img5 from '../assets/5.png';
+
+const headingOptions = {
+  pos: 'absolute',
+  left: '50%',
+  top: '50%',
+  transform: 'translate(-50%,-50%)',
+  textTransform: 'uppercase',
+  p: '4',
+  size: '4xl',
+};
+
+const Home = () => {
+  return (
+    <Box>
+      <MyCarousel />
+
+      <Container maxW={'container.xl'} minH={'100vh'} p="16">
+        <Heading
+          textTransform={'uppercase'}
+          py="2"
+          w={'fit-content'}
+          borderBottom={'2px solid'}
+          m="auto"
+        >
+          Services
+        </Heading>
+
+        <Stack
+          h="full"
+          p={'4'}
+          alignItems={'center'}
+          direction={['column', 'row']}
+        >
+          <Image src={img5} h={['40', '400']} filter={'hue-rotate(-130deg)'} />
+
+          <Text
+            letterSpacing={'widest'}
+            lineHeight={'190%'}
+            p={['4', '16']}
+            textAlign={'center'}
+          >
+            Explore our comprehensive range of services designed to empower your
+            online presence and propel your business to new heights. From
+            cutting-edge web development solutions that seamlessly blend form
+            and function, to personalized digital marketing strategies that
+            amplify your brand's reach and engagement, we're here to support
+            your success every step of the way. Elevate your online storefront
+            with our expertly crafted e-commerce solutions, tailored to meet
+            your unique business needs and drive conversions. Engage your
+            audience on the go with our captivating mobile app development
+            services, designed to deliver exceptional user experiences and
+            maximize customer satisfaction. Harness the power of data-driven
+            insights with our advanced analytics solutions, providing actionable
+            intelligence to inform your strategic decisions. Rest easy knowing
+            your digital assets are protected by our dedicated cybersecurity
+            experts, who work tirelessly to safeguard your business from
+            emerging threats. Embrace efficiency and scalability with our cloud
+            computing solutions, facilitating seamless collaboration and growth.
+            Plus, our reliable IT support services ensure your systems are
+            running smoothly around the clock, allowing you to focus on what
+            matters most - growing your business.
+          </Text>
+        </Stack>
+      </Container>
+    </Box>
+  );
+};
+
+const MyCarousel = () => (
+  <Carousel
+    autoPlay
+    infiniteLoop
+    interval={1000}
+    showStatus={false}
+    showThumbs={false}
+    showArrows={false}
+  >
+    <Box w="full" h={'100vh'}>
+      <Image src={img1} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
+        Watch The Future
+      </Heading>
+    </Box>
+    <Box w="full" h={'100vh'}>
+      <Image src={img2} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.900'} color={'black'} {...headingOptions}>
+        Future is Gaming
+      </Heading>
+    </Box>
+
+    <Box w="full" h={'100vh'}>
+      <Image src={img3} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+        Gaming on Console
+      </Heading>
+    </Box>
+
+    <Box w="full" h={'100vh'}>
+      <Image src={img4} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+        Night life is cool
+      </Heading>
+    </Box>
+  </Carousel>
+);
+
+export default Home;
